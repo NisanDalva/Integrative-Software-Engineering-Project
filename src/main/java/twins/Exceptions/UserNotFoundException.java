7e7e85d2397e333f2b963973ac165feb90580888
Nisan Dalva <nisan.dalva@s.afeka.ac.il>
2021-06-01 14:03:58 +0300

    fixed message error when status 404 occurred

--- src/main/java/twins/Exceptions/UserNotFoundException.java
+++ src/main/java/twins/Exceptions/UserNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -3852861819213897449L;
 
 	public UserNotFoundException() {
 	}
